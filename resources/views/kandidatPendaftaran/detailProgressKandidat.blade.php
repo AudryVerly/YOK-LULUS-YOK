30995ff2cbd9d2f4613045dea2232cd648ba2599
Audry Verly <audry.verly@gmail.com>
2026-05-10 02:09:05 +0700

    alert

--- resources/views/kandidatPendaftaran/detailProgressKandidat.blade.php
+++ resources/views/kandidatPendaftaran/detailProgressKandidat.blade.php
@@ -303,5 +303,23 @@
 
             });
         });
+
+        @if (session('success'))
+            Swal.fire({
+                icon: 'success',
+                title: 'Berhasil',
+                text: "{{ session('success') }}",
+                timer: 2500,
+                showConfirmButton: false
+            });
+        @elseif (session('error'))
+            Swal.fire({
+                icon: 'error',
+                title: 'Gagal',
+                text: "{{ session('error') }}",
+                timer: 2500,
+                showConfirmButton: false
+            });
+        @endif
     </script>
 @endpush
